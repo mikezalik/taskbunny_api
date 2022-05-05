2e4fdddaeb4dc4850cb1fce4d57bd8c590c7b6f7
mikezalik <mike.zalik@gmail.com>
2022-05-05 15:32:52 -0700

    Modifies foreignkeys.

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    task_id = db.relationship('Task', cascade='all, delete', backref='user', lazy=True)
+    tasks = db.relationship('Task', cascade='all, delete', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User: {self.User}"
