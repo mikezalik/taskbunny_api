435f6b2b2875aeeba1855b3cda366c8c275d7df0
mikezalik <mike.zalik@gmail.com>
2022-05-05 13:41:17 -0700

    test Heroku postgresql.

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import os
 
-from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, unset_jwt_cookies, jwt_required, JWTManager
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt,
+    get_jwt_identity,
+    unset_jwt_cookies,
+    jwt_required,
+    JWTManager,
+)
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +15,7 @@ from datetime import datetime, timedelta, timezone
 from flask_cors import CORS
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import urllib.request
@@ -19,7 +27,7 @@ CORS(app, support_credentials=True)
 # pg_db = os.getenv("PG_DB")
 # # heroku postgres/sql_alchemy compatibility hack
 # if pg_db.startswith("postgres://"):
-    # pg_db = pg_db.replace("postgres://", "postgresql://", 1)
+# pg_db = pg_db.replace("postgres://", "postgresql://", 1)
 secret = os.getenv("SECRET_KEY")
 salt = os.getenv("BCRYPT_SALT")
 
@@ -28,27 +36,28 @@ app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(app)
 
 # because heroku...
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://llecqiwurstram:29da2a1c1e6eebabd1f7502b11993b9da68b9b891c8b5ebed5f332526a236ad4@ec2-54-157-79-121.compute-1.amazonaws.com:5432/demlrful9pk9ne'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://llecqiwurstram:29da2a1c1e6eebabd1f7502b11993b9da68b9b891c8b5ebed5f332526a236ad4@ec2-54-157-79-121.compute-1.amazonaws.com:5432/demlrful9pk9ne"
 db = SQLAlchemy(app)
 CORS(app)
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(500), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    
+
     def __repr__(self):
         return f"Task: {self.task}"
 
     def __init__(self, task):
         self.task = task
 
+
 def format_task_json(task):
-    return {
-        "task": task.task,
-        "id": task.id,
-        "created_at": task.created_at
-    }
+    return {"task": task.task, "id": task.id, "created_at": task.created_at}
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -62,7 +71,7 @@ class User(db.Model):
         self.email = email
         self.password = password
         # Add Bcrypt when you have time...
-    
+
 
 # def _build_cors_preflight_response():
 #     response = make_response()
@@ -70,6 +79,7 @@ class User(db.Model):
 #     response.headers.add('Access-Control-Allow-Headers', "*")
 #     response.headers.add('Access-Control-Allow-Methods', "*")
 #     return response
+
 
 @app.after_request
 def refresh_expiring_jwts(response):
@@ -81,13 +91,14 @@ def refresh_expiring_jwts(response):
             access_token = create_access_token(identity=get_jwt_identity())
             data = response.get_json()
             if type(data) is dict:
-                data["access_token"] = access_token 
+                data["access_token"] = access_token
                 response.data = json.dumps(data)
         return response
     except (RuntimeError, KeyError):
         return response
 
-@app.route('/auth', methods=["POST"])
+
+@app.route("/auth", methods=["POST"])
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
@@ -102,18 +113,19 @@ def create_token():
     password = User.query.filter_by(password=password)
     if test and password:
         access_token = create_access_token(identity=email)
-        response = {"access_token":access_token}
+        response = {"access_token": access_token}
         return response
 
-@app.route('/register', methods=["POST"])
+
+@app.route("/register", methods=["POST"])
 def create_user():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
- 
+
     new_user = User(email, password)
-    db.session.add(new_user) 
-    db.session.commit()   
-    return jsonify({'message': 'registered successfully'})
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "registered successfully"})
 
 
 @app.route("/logout", methods=["POST"])
@@ -122,11 +134,12 @@ def logout():
     unset_jwt_cookies(response)
     return response
 
-@app.route('/tasks', methods=["POST", "GET"])
+
+@app.route("/tasks", methods=["POST", "GET"])
 def task():
     if request.method == "POST":
         # _build_cors_preflight_response()
-        task_name = request.json['task']
+        task_name = request.json["task"]
         task = Task(task_name)
         db.session.add(task)
         db.session.commit()
@@ -136,40 +149,42 @@ def task():
     task_list = []
     for task in tasks:
         task_list.append(format_task_json(task))
-    return {'tasks': task_list}
+    return {"tasks": task_list}
 
-@app.route('/tasks/<id>', methods=["PUT", "DELETE", "GET"])
+
+@app.route("/tasks/<id>", methods=["PUT", "DELETE", "GET"])
 def task_modification(id):
     if request.method == "PUT":
         # _build_cors_preflight_response()
         task_find = Task.query.filter_by(id=id)
-        task = request.json['task']
-        task_find.update(dict(task = task, created_at = datetime.utcnow()))
-        return {'task': format_task_json(task_find.one())}
-    
+        task = request.json["task"]
+        task_find.update(dict(task=task, created_at=datetime.utcnow()))
+        return {"task": format_task_json(task_find.one())}
+
     elif request.method == "DELETE":
-        
+
         task = Task.query.filter_by(id=id).one()
         db.session.delete(task)
         db.session.commit()
-        return 'Task Deleted!'
+        return "Task Deleted!"
 
     elif request.method == "GET":
         task = Task.query.filter_by(id=id).one()
         format_task = format_task_json(task)
-        return {'task': format_task}
+        return {"task": format_task}
 
     return "Task Not Found."
 
-@app.route('/quotes', methods=["GET"])
+
+@app.route("/quotes", methods=["GET"])
 def get_news():
     url = "http://programming-quotes-api.herokuapp.com/quotes/random"
-    
+
     response = urllib.request.urlopen(url)
     data = response.read()
 
-    return data;
+    return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
